bd0a9247266cd657812dadbcb9be039083a9b16b
RDRL <RDRL@DESKTOP-C49MABL>
2020-08-31 20:01:55 +0530

    First Commit

---
+++ BookingAndPassengerDetailsMicroService/src/main/java/com/cg/flight/dao/FlightDao.java
@@ -0,0 +1,19 @@
+package com.cg.flight.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.cg.flight.entity.Booking;
+import com.cg.flight.entity.Passenger;
+
+@Repository
+public interface FlightDao extends JpaRepository<Booking,Integer>{
+	@Query(value = "from Booking b inner join fetch b.user u where u.contactNo=:cnum")
+	public List<Booking> viewBookings(@Param("cnum") String num);
+	@Query(value = "from Passenger p inner join fetch p.booking b where b.bookingId=:bid")
+	public List<Passenger> viewPassengers(@Param("bid") String id);
+}
